4b5ea63a6b15a29f52df57335d724fbf5851cb1f
ivanchikap <anton.ivanchik@gmail.com>
2024-03-22 15:01:21 +0200

    start to work

---
+++ docs/assets/js/app.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",(()=>{const e=document.getElementById("burger"),t=(document.getElementById("sidebar"),document.body),d=document.getElementById("pageMask");e.addEventListener("click",(e=>{t.classList.toggle("show-sidebar")})),d.addEventListener("click",(e=>{t.classList.remove("show-sidebar")}))}));
