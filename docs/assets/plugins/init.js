e7eb532f1485f41dd281d12de37a75beaf72a38a
ivanchikap <anton.ivanchik@gmail.com>
2024-04-03 10:42:29 +0300

    finish tasks list

--- docs/assets/plugins/init.js
+++ docs/assets/plugins/init.js
@@ -1,3 +1,20 @@
 $(function(){
     $("#website-slider").twentytwenty();
+
+    $('.testimonial__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 577,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
+    });
 });
