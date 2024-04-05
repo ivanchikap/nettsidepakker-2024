3d4cead1286e929c79c21589e1668562b98ea018
ivanchikap <anton.ivanchik@gmail.com>
2024-04-05 14:13:59 +0300

    tips done

--- docs/assets/plugins/swiper-init.js
+++ docs/assets/plugins/swiper-init.js
@@ -1,6 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
     direction: "horizontal",
     slidesPerView: 3,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
     autoplay: {
         delay: 5000
     },
